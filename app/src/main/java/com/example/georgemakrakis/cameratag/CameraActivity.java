77c8d9c0904739c48f11ebcaa49e591d8ec08e55
georgemakrakis <georgemakrakis88@gmail.com>
2017-12-01 18:16:09 +0200

    Some first approach to getting location

--- app/src/main/java/com/example/georgemakrakis/cameratag/CameraActivity.java
+++ app/src/main/java/com/example/georgemakrakis/cameratag/CameraActivity.java
@@ -1,5 +1,6 @@
 package com.example.georgemakrakis.cameratag;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,11 +11,15 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.hardware.Camera;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -53,7 +58,7 @@ public class CameraActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        mImageView=(ImageView)findViewById(R.id.imageview1);
+        mImageView = (ImageView) findViewById(R.id.imageview1);
         //Through an exception, in this way the VM ignores the file URI exposure.
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
@@ -107,7 +112,7 @@ public class CameraActivity extends AppCompatActivity
     {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_"+n;
+        String imageFileName = "JPEG_" + timeStamp + "_" + n;
         File storageDir = Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
@@ -121,6 +126,50 @@ public class CameraActivity extends AppCompatActivity
         return image;
     }
 
+    private String getAddress()
+    {
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return TODO;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, true));
+        if(location!= null)
+        {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+
+
+            Geocoder geocoder;
+            List<Address> addresses;
+            geocoder = new Geocoder(this, Locale.getDefault());
+
+            try
+            {
+                addresses = geocoder.getFromLocation(lat, lng, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                String city = addresses.get(0).getLocality();
+                String country = addresses.get(0).getCountryName();
+                String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+                return city + " " + country + " " + knownName;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+
+            }
+        }
+        return "";
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -147,7 +196,7 @@ public class CameraActivity extends AppCompatActivity
                 int mHeight= this.getResources().getDisplayMetrics().heightPixels;
 
                 //Drawing text to bottom left corner
-                canvas.drawText(timeStamp.toString(),0, (mHeight*2)+paint.getTextSize(),paint);
+                canvas.drawText(timeStamp.toString()+""+getAddress(),0, (mHeight*2)+paint.getTextSize(),paint);
 
                 File image= createImageFile("new");
                 FileOutputStream out=new FileOutputStream(image);
